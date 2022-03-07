bdc2d4af204c3bef6b45e2dbd8d21e81708b0ae8
AndreiAndrewDrew <andreiandrewg@gmail.com>
2022-03-07 12:10:08 +0200

    creat proiect pendtru IDEA

--- AndrewSandbox/src/main/java/FirstProgram.java
+++ AndrewSandbox/src/main/java/FirstProgram.java
@@ -1,7 +1,7 @@
 public class FirstProgram {
 
-   public static void main(String[] args){
-     System.out.println("Hello, World!!!");
-   }
+    public static void main(String[] args) {
+        System.out.println("Hello Andrew, World!!!");
+    }
 
 }
